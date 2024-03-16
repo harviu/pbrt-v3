d43fc5fa236f79e2d9f42e7c529155d9134d3682
harviu <aide0740@gmail.com>
2024-03-15 16:26:04 -0900

    fix the initialization problem for atomic

--- src/globals.cpp
+++ src/globals.cpp
@@ -3,12 +3,12 @@
 #include <atomic>
 
 namespace pbrt {
-    std::atomic<int> lab2_total_rays = 0;
-    std::atomic<int> lab2_bbox_rays = 0;
-    std::atomic<int> lab2_object_rays = 0;
-    std::atomic<int> lab2_snowman_rays = 0;
-    std::atomic<int> lab2_body_rays = 0;
-    std::atomic<int> lab2_head_rays = 0;
+    std::atomic<int> lab2_total_rays(0);
+    std::atomic<int> lab2_bbox_rays(0);
+    std::atomic<int> lab2_object_rays(0);
+    std::atomic<int> lab2_snowman_rays(0);
+    std::atomic<int> lab2_body_rays(0);
+    std::atomic<int> lab2_head_rays(0);
     std::vector<int> tile_counter(0);
     int nTiles_x=0;
     int nTiles_y = 0;
